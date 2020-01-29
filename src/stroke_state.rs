e5e8f7f84092c7686c4b4783ddadc4f27c912581
messense <messense@icloud.com>
2020-01-29 13:28:11 +0800

    Impl StrokeState::dashes method

--- src/stroke_state.rs
+++ src/stroke_state.rs
@@ -1,8 +1,12 @@
+use std::convert::TryFrom;
+
 use mupdf_sys::*;
+use num_enum::TryFromPrimitive;
 
 use crate::{context, Error};
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, TryFromPrimitive)]
+#[repr(u32)]
 pub enum LineCap {
     Butt = 0,
     Round = 1,
@@ -10,12 +14,25 @@ pub enum LineCap {
     Triangle = 3,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+impl Default for LineCap {
+    fn default() -> Self {
+        Self::Butt
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, TryFromPrimitive)]
+#[repr(u32)]
 pub enum LineJoin {
     Miter = 0,
     Round = 1,
     Bevel = 2,
     MiterXps = 3,
+}
+
+impl Default for LineJoin {
+    fn default() -> Self {
+        Self::Miter
+    }
 }
 
 #[derive(Debug)]
@@ -25,10 +42,10 @@ pub struct StrokeState {
 
 impl StrokeState {
     pub fn new(
-        start_cap: u32,
-        dash_cap: u32,
-        end_cap: u32,
-        line_join: u32,
+        start_cap: LineCap,
+        dash_cap: LineCap,
+        end_cap: LineCap,
+        line_join: LineJoin,
         line_width: f32,
         miter_limit: f32,
         dash_phase: f32,
@@ -38,10 +55,10 @@ impl StrokeState {
         let inner = unsafe {
             ffi_try!(mupdf_new_stroke_state(
                 context(),
-                start_cap,
-                dash_cap,
-                end_cap,
-                line_join,
+                start_cap as fz_linecap,
+                dash_cap as fz_linecap,
+                end_cap as fz_linecap,
+                line_join as fz_linejoin,
                 line_width,
                 miter_limit,
                 dash_phase,
@@ -52,20 +69,41 @@ impl StrokeState {
         Ok(Self { inner })
     }
 
-    pub fn start_cap(&self) -> u32 {
-        unsafe { (*self.inner).start_cap }
+    pub fn try_clone(&self) -> Result<Self, Error> {
+        let start_cap = self.start_cap();
+        let dash_cap = self.dash_cap();
+        let end_cap = self.end_cap();
+        let line_join = self.line_join();
+        let line_width = self.line_width();
+        let miter_limit = self.miter_limit();
+        let dash_phase = self.dash_phase();
+        let dashes = self.dashes();
+        Self::new(
+            start_cap,
+            dash_cap,
+            end_cap,
+            line_join,
+            line_width,
+            miter_limit,
+            dash_phase,
+            &dashes,
+        )
     }
 
-    pub fn dash_cap(&self) -> u32 {
-        unsafe { (*self.inner).dash_cap }
+    pub fn start_cap(&self) -> LineCap {
+        LineCap::try_from(unsafe { (*self.inner).start_cap }).unwrap()
     }
 
-    pub fn end_cap(&self) -> u32 {
-        unsafe { (*self.inner).end_cap }
+    pub fn dash_cap(&self) -> LineCap {
+        LineCap::try_from(unsafe { (*self.inner).dash_cap }).unwrap()
     }
 
-    pub fn line_join(&self) -> u32 {
-        unsafe { (*self.inner).linejoin }
+    pub fn end_cap(&self) -> LineCap {
+        LineCap::try_from(unsafe { (*self.inner).end_cap }).unwrap()
+    }
+
+    pub fn line_join(&self) -> LineJoin {
+        LineJoin::try_from(unsafe { (*self.inner).linejoin }).unwrap()
     }
 
     pub fn line_width(&self) -> f32 {
@@ -81,7 +119,12 @@ impl StrokeState {
     }
 
     pub fn dashes(&self) -> Vec<f32> {
-        todo!()
+        unsafe {
+            let dash_len = (*self.inner).dash_len as usize;
+            let mut dash_list = Vec::with_capacity(dash_len);
+            dash_list.extend_from_slice(&(*self.inner).dash_list[0..dash_len]);
+            dash_list
+        }
     }
 }
 
@@ -92,5 +135,11 @@ impl Drop for StrokeState {
                 fz_drop_stroke_state(context(), self.inner);
             }
         }
+    }
+}
+
+impl Clone for StrokeState {
+    fn clone(&self) -> StrokeState {
+        self.try_clone().unwrap()
     }
 }
