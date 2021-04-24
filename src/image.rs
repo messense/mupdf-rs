21aaf6d5729e56f408f8ce53bbcbd8697437932f
messense <messense@icloud.com>
2021-04-24 11:47:36 +0800

    Fix PdfDocument::find_page double-free bug

--- src/image.rs
+++ src/image.rs
@@ -114,8 +114,6 @@ impl Drop for Image {
 
 impl Clone for Image {
     fn clone(&self) -> Self {
-        unsafe {
-            Image::from_raw(fz_keep_image(context(), self.inner))
-        }
+        unsafe { Image::from_raw(fz_keep_image(context(), self.inner)) }
     }
 }
