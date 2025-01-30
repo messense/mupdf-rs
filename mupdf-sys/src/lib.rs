b60525b19965aaa39a01809b09c6dcd8c4f9f667
ginnyTheCat <ginnythecat@lelux.net>
2025-01-30 21:56:23 +0100

    Start implementing custom devices

--- mupdf-sys/src/lib.rs
+++ mupdf-sys/src/lib.rs
@@ -4,3 +4,13 @@
 #![allow(clippy::all)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub unsafe fn mupdf_new_derived_device<T>(
+    ctx: *mut fz_context,
+    label: *const std::ffi::c_char,
+) -> *mut T {
+    let size = std::ffi::c_int::try_from(size_of::<T>()).unwrap();
+    let device = fz_new_device_of_size(ctx, size);
+    let label = Memento_label(device.cast(), label);
+    label.cast()
+}
