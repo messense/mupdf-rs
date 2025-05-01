967e82e005a663d95efa7d6645ec4369f95a2046
Yves Zoundi <yveszoundi@gmail.com>
2025-05-01 14:01:35 -0400

    CI disable some flags on windows

--- mupdf-sys/build.rs
+++ mupdf-sys/build.rs
@@ -514,10 +514,12 @@ fn main() {
     println!("cargo:rerun-if-changed=wrapper.c");
 
     if let Ok(ref target_os) = env::var("CARGO_CFG_TARGET_OS") {
-        if target_os == "macos" {
-            println!("cargo:rustc-link-lib=c++");
-        } else {
-            println!("cargo:rustc-link-lib=stdc++");
+        if target_os != "windows" {
+            if target_os == "macos" {
+                println!("cargo:rustc-link-lib=c++");
+            } else {
+                println!("cargo:rustc-link-lib=stdc++");
+            }
         }
     }
 
