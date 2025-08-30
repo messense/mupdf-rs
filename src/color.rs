7741da3ebdc2ed064e314cf74010eef33076b86d
itsjunetime <junewelker@gmail.com>
2025-08-30 09:32:44 -0500

    isolate Color conversions into pub(crate) usage-specific fns

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,4 @@
+#[derive(Copy, Clone)]
 pub struct Color {
     pub alpha: u8,
     pub red: u8,
@@ -5,6 +6,8 @@ pub struct Color {
     pub blue: u8,
 }
 
+// We'll be using this in a later PR, just adding the struct right now
+#[expect(dead_code)]
 impl Color {
     fn into_bytes(self) -> [u8; 4] {
         let Color {
@@ -15,11 +18,13 @@ impl Color {
         } = self;
         [alpha, red, green, blue]
     }
-}
 
-impl From<f32> for Color {
-    fn from(value: f32) -> Self {
-        let [alpha, red, green, blue] = value.to_be_bytes();
+    pub(crate) fn into_mupdf_float(self) -> f32 {
+        f32::from_be_bytes(self.into_bytes())
+    }
+
+    pub(crate) fn from_mupdf_float(f: f32) -> Self {
+        let [alpha, red, green, blue] = f.to_be_bytes();
         Self {
             alpha,
             red,
@@ -27,29 +32,19 @@ impl From<f32> for Color {
             blue,
         }
     }
-}
 
-impl From<Color> for f32 {
-    fn from(value: Color) -> Self {
-        Self::from_be_bytes(value.into_bytes())
+    pub(crate) fn into_mupdf_int(self) -> i32 {
+        i32::from_be_bytes(self.into_bytes())
     }
-}
 
-impl From<i32> for Color {
-    fn from(value: i32) -> Self {
-        let [alpha, red, green, blue] = value.to_be_bytes();
+    pub(crate) fn from_mupdf_int(i: i32) -> Self {
+        let [alpha, red, green, blue] = i.to_be_bytes();
         Self {
             alpha,
             red,
             green,
             blue,
         }
-    }
-}
-
-impl From<Color> for i32 {
-    fn from(value: Color) -> Self {
-        Self::from_be_bytes(value.into_bytes())
     }
 }
 
