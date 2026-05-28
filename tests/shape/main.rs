778023cb565c455fbadccb65ce2a1d7f762b53ba
messense <messense@icloud.com>
2026-05-28 19:35:25 +0800

    Remove path attributes from shape tests

---
+++ tests/shape/main.rs
@@ -0,0 +1,17 @@
+mod red_rect;
+
+mod basic_drawing;
+
+mod shape_commit;
+
+mod arc_curve_primitives;
+
+mod insert_text;
+
+mod justify_alignment;
+
+mod opacity_extgstate;
+
+mod custom_fonts;
+
+mod kitchen_sink;
